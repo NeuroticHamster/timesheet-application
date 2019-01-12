48786b991b5f62c01cb603844be587e4f32b3b34
rich bullis <richbullis@gmail.com>
2019-01-12 18:39:47 -0500

    Limited the application to one start time and one end time a day. Added Title bar

--- timesheetz/main.py
+++ timesheetz/main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, String, Integer, Column, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-
+import time
 engine = create_engine('sqlite:///timesheetz.db:')
 base = declarative_base(engine)
 
@@ -13,36 +13,52 @@ base = declarative_base(engine)
 class times(base):
 	__tablename__ = 'times'
 	id = Column(Integer, primary_key='True')
-	start_time = Column(DateTime)
-	end_time = Column(DateTime)
-	date = Column(DateTime)
+	start_time = Column(String)
+	end_time = Column(String)
+	date = Column(String)
 	
 
 base.metadata.create_all(engine)
 base.metadata.bind = engine
 session = sessionmaker()
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/', methods=['GET', 'POST'])
 def basic_route():
 	sqlsession = session()
 
 	wdate = sqlsession.query(times.date).all()
+	wtime = sqlsession.query(times.start_time).all()
+	etime = sqlsession.query(times.end_time).all()
 
 	checkdate = datetime.now().date()
 	
 	if request.method == 'POST':
-		for item in wdate:
-			if item == str(checkdate):
 	
-				svalue = times(start_time=datetime.now(), date=datetime.now().date())
-				sqlsession.add(svalue)
-				sqlsession.commit()
-			else:
-				flash('Already added')
-				print('nope')
-    
-	return render_template('index.html', wdate=wdate)
+		
+		convertDate = time.strptime(str(datetime.now().date()).replace('-', ' '), "%Y %m %d")
+		convertTime = time.strptime(str(datetime.now().time()).replace(':', ' ').replace('.', ' '), "%H %M %S %f")
+		year, month, day = convertDate[0:3]
+		hour, minutes = convertTime[3:5]
+		convertDate = str(year) + ' ' + str(month) + ' ' + str(day)
+	
+		convertTime = str(hour) + ': ' + str(minutes)
+		if str(convertDate) not in str(wdate):
+			svalue = times(start_time=convertTime, date=convertDate)
+			sqlsession.add(svalue)
+			sqlsession.commit()
+			print('added')
+		elif str(convertDate) in str(wdate) and str(*sqlsession.query(times.start_time)[-1]) != str(None):
+			print(str(*sqlsession.query(times.start_time)[-1]))
+			print('oh shit')
+			svalue = times(end_time=convertTime, date=convertDate)
+			sqlsession.add(svalue)
+			sqlsession.commit()
+		else:
+			flash('Already added')
+			print('nope')
+
+	return render_template('index.html', wdate=wdate, wtime=wtime, etime=etime)
 @app.route('/overview')
 def overview():
 	return 'hi'
